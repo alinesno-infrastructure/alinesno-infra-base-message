c5a9c4cfa2561bdda3006cddc97d7e31fde35e44
switchII <luoandon@gmail.com>
2023-11-07 21:59:53 +0800

    feat:add pages

---
+++ alinesno-infra-base-message-facade/src/main/java/com/alinesno/infra/base/message/service/IMqApplicationService.java
@@ -0,0 +1,7 @@
+package com.alinesno.infra.base.message.service;
+
+import com.alinesno.infra.base.message.entity.MqApplicationEntity;
+import com.alinesno.infra.common.facade.services.IBaseService;
+
+public interface IMqApplicationService extends IBaseService<MqApplicationEntity> {
+}
